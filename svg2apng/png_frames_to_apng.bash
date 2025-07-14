23ab281dfb95da715167cb324989b50dceebb829
Tim Schäfer <tim.schaefer@ae.mpg.de>
2025-07-14 11:26:35 +0200

    NEW: support setting input file suffix or entire pattern on command line for pngframes2apng script

--- svg2apng/png_frames_to_apng.bash
+++ svg2apng/png_frames_to_apng.bash
@@ -16,6 +16,8 @@ framerate=10
 output_file="animated.png"
 input_dir="./"
 input_file_prefix="frame_"
+input_file_suffix="%3d.png"
+input_file_pattern=""
 
 # Function to display help message
 show_help() {
@@ -26,11 +28,18 @@ show_help() {
     echo "* --framerate: The number of frames per second in the output animation. Positive integer, defaults to 10."
     echo "* --outputfile: The name of the output APNG file. Defaults to 'animated.png'."
     echo "* --inputdir: The directory containing the input PNG frames. Defaults to the current directory. Unless you also set --inputfileprefix, the prefix defaults to 'frame_', so the frames in the directory must be named 'frame_001.png', 'frame_002.png', etc. (You can start with index 000 or 001.)"
-    echo "* --inputfileprefix <prefix>: set input files prefix to <prefix>. The suffix '%3d.png' will be appended to look for the set of input files. If omitted, defaults to 'frame_'."
+    echo "* --inputfilepattern <pattern>: The pattern to match input files within inputdir. If set, it overrides --inputfileprefix and --inputfilesuffix. The pattern should be a valid ffmpeg pattern, e.g., 'frame_%3d.png'." for file names like frame_001.png, frame_002.png, etc.
+    echo "* --inputfileprefix <prefix>: set input files prefix to <prefix>. The default suffix '%3d.png' will be appended to look for the set of input files, unless --inputfilesuffix is also given. If omitted, defaults to 'frame_'. See also --inputfilepattern as an alternative way to set both prefix and suffix at once."
+    echo "* --inputfilesuffix <suffix>: set input files suffix to <suffix>. Defaults to '%3d.png'. The default prefix 'frame_' will be prepended, unles --inputfileprefix is also given. If omitted, defaults to '%3d.png'. See also --inputfilepattern as an alternative way to set both prefix and suffix at once."
     echo "* --help: Show this help message."
     echo "Example: $0 --framerate 10 --outputfile animation_out.png --inputdir ./frames_tmp"
     exit
 }
+
+if [ -z "$1" ]; then
+    echo "No arguments provided. Use --help to see usage."
+    exit 1
+fi
 
 # Parse named arguments
 while [[ "$#" -gt 0 ]]; do
@@ -52,10 +61,18 @@ while [[ "$#" -gt 0 ]]; do
             input_dir="$2"
             shift
             ;;
-	--inputfileprefix)
-	    input_file_prefix="$2"
-	    shift
-	    ;;
+        --inputfileprefix)
+            input_file_prefix="$2"
+            shift
+            ;;
+        --inputfilesuffix)
+            input_file_suffix="$2"
+            shift
+            ;;
+        --inputfilepattern)
+            input_file_pattern="$2"
+            shift
+            ;;
         --help)
             show_help
             ;;
@@ -71,9 +88,16 @@ done
 echo "Frame rate: $framerate"
 echo "Output file: $output_file"
 echo "Input directory: $input_dir"
-echo "Input file prefix: $input_file_prefix"
 
+# If --inputfilepattern is not set, construct the input file pattern from prefix and suffix.
+if [ -z "$input_file_pattern" ]; then
+    echo "Input file prefix: $input_file_prefix"
+    echo "Input file suffix: $input_file_suffix"
+    input_file_pattern="${input_dir}/${input_file_prefix}{$input_file_suffix}"
+fi
+
+echo "Input file pattern: ${input_file_pattern}"
 
 echo "Converting frames in directory '${input_dir}' to output file '${output_file}' with framerate ${framerate}..."
-ffmpeg -y -framerate $framerate -i "${input_dir}/${input_file_prefix}%3d.png" -plays 0 -vf "fps=${framerate}" -f apng "${output_file}"
+ffmpeg -y -framerate $framerate -i "${input_dir}/${input_file_pattern}" -plays 0 -vf "fps=${framerate}" -f apng "${output_file}"
 echo "Conversion complete. Check output file, e.g. run: 'firefox ${output_file}'"
